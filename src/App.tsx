fcd541ad683aa3b178d7413bc063fe3cbba6c58a
Giuliano Espejo Mezzabotta <129322090+Giuliano-Espejo@users.noreply.github.com>
2024-06-13 23:38:01 -0300

    add restriccion por horario

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './theme/theme';
 import Rutas from './Routes/Rutas';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/store';
+import './App.css'; // Asegúrate de importar tus estilos
 
 const App: React.FC = () => {
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    if (!isWithinBusinessHours()) {
+      setShowAlert(true);
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Provider store={store}>
-      <Rutas/>
+        {showAlert ? (
+          <div className="alert">
+            Fuera del horario de atención. Nuestro horario de atención es de lunes a viernes de 12:00 a 20:00, y de sábados a domingos de 11:00 a 15:00.
+          </div>
+        ) : (
+          <Rutas />
+        )}
       </Provider>
     </ThemeProvider>
   );
+};
+
+const isWithinBusinessHours = () => {
+  const now = new Date();
+  const day = now.getDay();
+  const hour = now.getHours();
+  const minutes = now.getMinutes();
+
+  if (day >= 1 && day <= 5) { // Lunes a viernes
+    return (hour > 12 || (hour === 12 && minutes >= 0)) && (hour < 20 || (hour === 20 && minutes === 0));
+  } else { // Sábado y domingo
+    return (hour > 11 || (hour === 11 && minutes >= 0)) && (hour < 15 || (hour === 15 && minutes === 0));
+  }
 };
 
 export default App;
